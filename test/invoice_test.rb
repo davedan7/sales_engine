a41312129363d90e1d4a145bc1207bd13e56fc8e
David Daniel <davedan7@gmail.com>
2015-03-15 16:17:43 -0600

    Finished modifying tests

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -7,12 +7,12 @@ require_relative '../lib/invoice'
 class InvoicesTest < Minitest::Test
 
   def setup
-    @data = { id: "2",
-              customer_id: "1",
-              merchant_id: "75",
-              status: "shipped",
-              created_at: "2012-03-25 09:54:09 UTC",
-              updated_at: "2012-03-25 09:54:09 UTC", }
+    @data = { id:          2,
+              customer_id: 1,
+              merchant_id: 75,
+              status:      "shipped",
+              created_at:  "2012-03-25 09:54:09 UTC",
+              updated_at:  "2012-03-25 09:54:09 UTC", }
   end
 
   def test_invoice_has_id
@@ -28,28 +28,24 @@ class InvoicesTest < Minitest::Test
   end
 
   def test_invoice_has_merchant_id
-    skip
     invoice = Invoice.new(@data, nil)
 
     assert_equal 75, invoice.merchant_id
   end
 
   def test_invoice_has_status
-    skip
     invoice = Invoice.new(@data, nil)
 
     assert_equal "shipped", invoice.status
   end
 
   def test_invoice_has_date_of_creation_in_yyyymmdd_hhmmss_format
-    skip
     invoice = Invoice.new(@data, nil)
 
     assert_equal "2012-03-25 09:54:09 UTC", invoice.created_at
   end
 
   def test_invoice_has_update_date_in_yyyymmdd_hhmmss_format
-    skip
     invoice = Invoice.new(@data, nil)
 
     assert_equal "2012-03-25 09:54:09 UTC", invoice.updated_at
--- test/transactions_test.rb
+++ test/transactions_test.rb
@@ -7,52 +7,47 @@ require_relative '../lib/transaction'
 class TransactionTest < Minitest::Test
 
   def setup
-    @data = { id: 2,
-              invoice_id: 2,
-              credit_card_number: 4580251236515201,
-              credit_card_expiration_date: ,
-              result: "success",
-              created_at: "2012-03-27 14:54:09 UTC",
-              updated_at: "2012-03-27 14:54:09 UTC" }
+    @data = { id:                          2,
+              invoice_id:                  2,
+              credit_card_number:          4580251236515201,
+              credit_card_expiration_date: "" ,
+              result:                      "success",
+              created_at:                  "2012-03-27 14:54:09 UTC",
+              updated_at:                  "2012-03-27 14:54:09 UTC" }
   end
 
   def test_transaction_has_id
-    transaction = Transaction.new(data, nil)
+    transaction = Transaction.new(@data, nil)
 
     assert_equal 2, transaction.id
   end
 
   def test_transaction_has_invoice_id
-    skip
-    transaction = Transaction.new(data, nil)
+    transaction = Transaction.new(@data, nil)
 
     assert_equal 2, transaction.invoice_id
   end
 
   def test_transaction_has_credit_card_number
-    skip
-    transaction = Transaction.new(data, nil)
+    transaction = Transaction.new(@data, nil)
 
     assert_equal 4580251236515201, transaction.credit_card_number
   end
 
   def test_transaction_has_credit_card_number_fail_success_outcome
-    skip
-    transaction = Transaction.new(data, nil)
+    transaction = Transaction.new(@data, nil)
 
     assert_equal "success", transaction.result
   end
 
   def test_transaction_has_date_of_creation_in_yyyymmdd_hhmmss_format
-    skip
-    transaction = Transaction.new(data,nil)
+    transaction = Transaction.new(@data,nil)
 
     assert_equal "2012-03-27 14:54:09 UTC", transaction.created_at
   end
 
   def test_transaction_has_update_date_in_yyyymmdd_hhmmss_format
-    skip
-    transaction = Transaction.new(data, nil)
+    transaction = Transaction.new(@data, nil)
 
     assert_equal "2012-03-27 14:54:09 UTC", transaction.updated_at
   end
