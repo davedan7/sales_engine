3040e2613486b7d066139fb75746a38cad2e866e
genekhanutin <genekhanutin@gmail.com>
2015-03-17 09:31:45 -0600

    merchant repo tests pass

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -17,6 +17,7 @@ class MerchantRepositoryTest < Minitest::Test
   def test_it_knows_its_parent
     engine = SalesEngine.new("stub")
     merchant_repo = MerchantRepository.new(@fixtures, engine)
+    
     assert_equal engine, merchant_repo.engine
   end
 
@@ -27,24 +28,27 @@ class MerchantRepositoryTest < Minitest::Test
 
   def test_it_returns_all_merchants
     merchant_repo = MerchantRepository.new(@fixtures, nil)
+    
     assert merchant_repo.merchants
   end
 
   def test_it_returns_random_merchants
     merchant_repo = MerchantRepository.new(@fixtures, nil)
+    
     assert merchant_repo.random
   end
 
   def test_it_can_load_merchants_from_csv
     merchant_repo = MerchantRepository.new(@fixtures, nil)
     assert_equal "Schroeder-Jerde", merchant_repo.merchants.first.name
+
     assert_equal "Bechtelar, Jones and Stokes", merchant_repo.merchants.last.name
   end
 
   def test_it_can_find_merchants_by_name
     merchant_repo = MerchantRepository.new(@fixtures, nil)
-    found = merchant_repo.find_by_name("Bechtelar, Jones and Stoke")
-    assert_equal 10, found.name
-  end
+    found         = merchant_repo.find_by_name("Bechtelar, Jones and Stokes")
 
+    assert_equal 10, found.id
+  end
 end
